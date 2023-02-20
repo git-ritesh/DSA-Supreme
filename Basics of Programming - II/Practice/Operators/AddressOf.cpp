9f2a5f6201023b601042a9bef565ababc12e966f
git-ritesh <stackritesh@gmail.com>
2023-02-21 04:27:27 +0530

    added address-of program

---
+++ Basics of Programming - II/Practice/Operators/AddressOf.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n = 10; 
+ // prints address of variable
+  cout << "Address of n is: " << &n << endl; // output will be like 0x61ff0c 
+
+return 0;
+}
