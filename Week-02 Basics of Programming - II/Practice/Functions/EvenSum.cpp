f3d587cf949ddb69c60cbf20a3488789cb3a79de
git-ritesh <stackritesh@gmail.com>
2023-02-21 22:05:23 +0530

    created EvenSum.cpp

--- Week-02 Basics of Programming - II/Practice/Functions/EvenSum.cpp
+++ Week-02 Basics of Programming - II/Practice/Functions/EvenSum.cpp
@@ -1,11 +1,20 @@
 #include<iostream>
 using namespace std;
 
+// function to evaluate evensum
+int evenSum( int n ) {
+  int sum = 0;
+  for ( int i = 2; i <= n; i=i+2 ) {
+    sum += i;
+  }
+  return sum;
+  }
+
 int main() {
   int n; 
   cout << "Enter a no. up to which you want to obtain even sum b/w them: " ;
   cin >> n; // takes number as input
-  
-
+  int sum = evenSum(n);
+  cout << "Sum of even no. b/w "<< 1 << " and " << n << " is: " << sum << endl;
 return 0;
 }
