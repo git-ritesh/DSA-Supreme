9b9dd300e7b2f4bb8935c5ef3eec367113d78b6c
git-ritesh <stackritesh@gmail.com>
2023-02-21 18:54:25 +0530

    modifying the directory's name

---
+++ Week-02 Basics of Programming - II/Practice/Functions/EvenSum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int n; 
+  cout << "Enter a no. up to which you want to obtain even sum b/w them: " ;
+  cin >> n; // takes number as input
+  
+
+return 0;
+}
