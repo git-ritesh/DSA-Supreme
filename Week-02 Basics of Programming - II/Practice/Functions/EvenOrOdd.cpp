991b1ad0dd046f9d8d9765ca590645e3d4027836
git-ritesh <stackritesh@gmail.com>
2023-02-21 23:44:17 +0530

    EvenOrOdd.cpp

---
+++ Week-02 Basics of Programming - II/Practice/Functions/EvenOrOdd.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+bool evenOdd( int num ) {
+  if ( num%2 == 0)
+    return true;
+  else 
+    return false;
+}
+// Program start here
+int main() {
+  int n;
+  cout << "Enter a no. to check Even Or Odd: ";
+  cin >> n ;
+  if(evenOdd(n)) {
+    cout << n << " is Even.";
+  }
+  else {
+    cout << n << " is Odd.";
+  }
+
+return 0;
+}
